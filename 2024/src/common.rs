36e00e7e7d4d1ace86d41eab5c63ae39a23f2fef
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2024-12-11 10:52:33 +0100

    Move Coord, Direction and MazePoint to common

--- 2024/src/common.rs
+++ 2024/src/common.rs
@@ -5,3 +5,75 @@ pub fn prnt_lines(lines: &Vec<Vec<char>>) {
         println!("{}", line);
     }
 }
+
+pub type Lines = Vec<Vec<char>>;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Direction {
+    pub dir: char,
+}
+
+impl Direction {
+    const VALID_DIRECTION: [char; 4] = ['^', '>', 'v', '<'];
+
+    pub fn new() -> Direction {
+        return Direction { dir: '.' };
+    }
+
+    pub fn is_valid(&self) -> bool {
+        return Direction::VALID_DIRECTION.contains(&self.dir);
+    }
+
+    pub fn next(&self) -> (isize, isize) {
+        assert!(self.is_valid());
+        match self.dir {
+            '^' => (-1, 0),
+            '>' => (0, 1),
+            'v' => (1, 0),
+            '<' => (0, -1),
+            _ => (0, 0),
+        }
+    }
+
+    pub fn turn_right(&mut self) {
+        assert!(self.is_valid());
+        self.dir = match self.dir {
+            '^' => '>',
+            '>' => 'v',
+            'v' => '<',
+            '<' => '^',
+            _ => '!',
+        }
+    }
+
+    pub fn turn_left(&mut self) {
+        assert!(self.is_valid());
+        self.dir = match self.dir {
+            '^' => '<',
+            '>' => '^',
+            'v' => '>',
+            '<' => 'v',
+            _ => '!',
+        }
+    }
+
+    pub fn from_dir(dir: &char) -> Option<Direction> {
+        if Direction::VALID_DIRECTION.contains(dir) {
+            return Some(Direction { dir: *dir });
+        }
+        return None;
+    }
+}
+
+#[derive(PartialEq, Hash, Eq)]
+pub struct Coord {
+    pub i: usize,
+    pub j: usize,
+}
+
+#[derive(PartialEq, Hash, Eq)]
+pub struct MazePoint {
+    pub i: usize,
+    pub j: usize,
+    pub dir: Direction,
+}
--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -1,28 +1,29 @@
+use crate::common::{Coord, Direction, MazePoint};
 use crate::day4;
+use std::collections::HashSet;
 use std::io;
 use std::process::exit;
-
-const VALID_DIRECTION: [char; 4] = ['^', '>', 'v', '<'];
 
 fn count_route(lines: &Vec<Vec<char>>) -> u32 {
     let mut maze = lines.to_vec();
     let mut positions: u32 = 0;
     let mut i: usize = 0;
     let mut j: usize = 0;
-    let mut dir = '.';
+    let mut dir: Direction = Direction::new();
     // find starting point
     for r in 0..maze.len() {
         for c in 0..maze[r].len() {
-            if VALID_DIRECTION.contains(&maze[r][c]) {
+            if let Some(_dir) = Direction::from_dir(&maze[r][c]) {
                 (i, j) = (r, c);
-                dir = maze[r][c]
+                dir = _dir;
+                break;
             }
         }
     }
-    while VALID_DIRECTION.contains(&dir) {
+    while dir.is_valid() {
         let di: isize;
         let dj: isize;
-        (di, dj) = direction_coordinates(dir);
+        (di, dj) = dir.next();
         if maze[i][j] != 'X' {
             positions += 1;
             maze[i][j] = 'X';
@@ -35,7 +36,7 @@ fn count_route(lines: &Vec<Vec<char>>) -> u32 {
         let ni = ni.expect("Checked it's correct");
         let nj = nj.expect("Checked it's correct");
         if maze[ni][nj] == '#' {
-            dir = new_direction(dir);
+            dir.turn_right();
             continue;
         }
         (i, j) = (ni, nj);
@@ -47,64 +48,60 @@ fn count_route(lines: &Vec<Vec<char>>) -> u32 {
     return positions;
 }
 
-#[derive(PartialEq)]
-struct Coord {
-    i: usize,
-    j: usize,
-    dir: char,
-}
-
 fn count_obstacles(lines: &Vec<Vec<char>>) -> u32 {
     let mut maze = lines.to_vec();
     let mut obstacles: u32 = 0;
     let mut i: usize = 0;
     let mut j: usize = 0;
-    let mut dir = '.';
+    let mut dir: Direction = Direction::new();
     // find starting point
     for r in 0..maze.len() {
         for c in 0..maze[r].len() {
-            if VALID_DIRECTION.contains(&maze[r][c]) {
+            if let Some(_dir) = Direction::from_dir(&maze[r][c]) {
                 (i, j) = (r, c);
-                dir = maze[r][c]
+                dir = _dir;
+                break;
             }
         }
     }
-    let mut obstacle_coordinates: Vec<Coord> = Vec::new();
-    while VALID_DIRECTION.contains(&dir) {
+    let mut obstacle_coordinates: HashSet<Coord> = HashSet::new();
+    let mut visited_coordinates: HashSet<Coord> = HashSet::new();
+    while dir.is_valid() {
         let di: isize;
         let dj: isize;
-        (di, dj) = direction_coordinates(dir);
+        (di, dj) = dir.next();
         let ni = i.checked_add_signed(di);
         let nj = j.checked_add_signed(dj);
         if ni.is_none() || Some(maze.len()) <= ni || nj.is_none() || Some(maze[i].len()) <= nj {
             break;
         }
+        visited_coordinates.insert(Coord { i, j });
         let ni = ni.expect("Checked it's correct");
         let nj = nj.expect("Checked it's correct");
         // loop detection
 
         let mut int_maze = maze.to_vec();
         let mut changed: bool = false;
-        let obstacle_coordinate = Coord {
-            i: ni,
-            j: nj,
-            dir: '.',
-        };
-        if int_maze[ni][nj] != '#' && !obstacle_coordinates.contains(&obstacle_coordinate) {
+        let obstacle_coordinate = Coord { i: ni, j: nj };
+        if int_maze[ni][nj] != '#'
+            && !obstacle_coordinates.contains(&obstacle_coordinate)
+            && !visited_coordinates.contains(&obstacle_coordinate)
+        {
             int_maze[ni][nj] = '#';
-            obstacle_coordinates.push(obstacle_coordinate);
+            obstacle_coordinates.insert(obstacle_coordinate);
             changed = true;
         }
-        let mut int_dir = new_direction(dir);
+        let mut int_dir = dir.clone();
+        int_dir.turn_right();
         let mut int_i = i;
         let mut int_j = j;
-        let mut coordinates: Vec<Coord> = Vec::new();
-        while VALID_DIRECTION.contains(&int_dir) && changed {
+        let mut coordinates: HashSet<MazePoint> = HashSet::new();
+        while int_dir.is_valid() && changed {
             int_maze[int_i][int_j] = 'X';
-            let current_coordinate = Coord {
+            let current_coordinate = MazePoint {
                 i: int_i,
                 j: int_j,
-                dir: int_dir,
+                dir: int_dir.clone(),
             };
             if coordinates.contains(&current_coordinate) {
                 for row in int_maze {
@@ -114,10 +111,10 @@ fn count_obstacles(lines: &Vec<Vec<char>>) -> u32 {
                 obstacles += 1;
                 break;
             }
-            coordinates.push(current_coordinate);
+            coordinates.insert(current_coordinate);
             let di: isize;
             let dj: isize;
-            (di, dj) = direction_coordinates(int_dir);
+            (di, dj) = int_dir.next();
             let ni = int_i.checked_add_signed(di);
             let nj = int_j.checked_add_signed(dj);
             if ni.is_none()
@@ -131,7 +128,7 @@ fn count_obstacles(lines: &Vec<Vec<char>>) -> u32 {
             let ni = ni.expect("Checked it's correct");
             let nj = nj.expect("Checked it's correct");
             if int_maze[ni][nj] == '#' {
-                int_dir = new_direction(int_dir);
+                int_dir.turn_right();
                 continue;
             }
             (int_i, int_j) = (ni, nj);
@@ -140,39 +137,17 @@ fn count_obstacles(lines: &Vec<Vec<char>>) -> u32 {
         //
         // continue normal route
         if maze[ni][nj] == '#' {
-            dir = new_direction(dir);
+            dir.turn_right();
             continue;
         }
         (i, j) = (ni, nj);
-        maze[ni][nj] = dir;
+        maze[ni][nj] = dir.dir;
     }
     for row in maze {
         let row: String = row.iter().collect();
         println!("{}", row);
     }
     return obstacles;
-}
-
-fn new_direction(dir: char) -> char {
-    assert!(VALID_DIRECTION.contains(&dir));
-    match dir {
-        '^' => '>',
-        '>' => 'v',
-        'v' => '<',
-        '<' => '^',
-        _ => '!',
-    }
-}
-
-fn direction_coordinates(dir: char) -> (isize, isize) {
-    assert!(VALID_DIRECTION.contains(&dir));
-    match dir {
-        '^' => (-1, 0),
-        '>' => (0, 1),
-        'v' => (1, 0),
-        '<' => (0, -1),
-        _ => (0, 0),
-    }
 }
 
 pub fn run(args: &[String]) -> io::Result<()> {
